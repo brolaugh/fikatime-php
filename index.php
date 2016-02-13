22e927d91f67b73c1f28ac2e2c77a0d482fcedf3
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-02-13 20:12:03 +0100

    Added basic HTML imports to index
    Signed-off-by: Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>

--- index.php
+++ index.php
@@ -8,3 +8,28 @@
 include_once ("Helper.php");
 
 $s = new com\brolaugh\database\Select();
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/bootstrap.min.css" charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap-material-design.min.css" charset="utf-8">
+    <link rel="stylesheet" href="css/ripples.min.css" charset="utf-8">
+    <meta charset="utf-8">
+    <title>FikaTime</title>
+  </head>
+  <body>
+
+    <main>
+
+    </main>
+    <footer>
+      <script src="js/jquery.min.js" charset="utf-8"></script>
+      <script src="js/bootstrap.min.js" charset="utf-8"></script>
+      <script src="js/material.min.js" charset="utf-8"></script>
+      <script src="js/ripples.min.js" charset="utf-8"></script>
+    </footer>
+  </body>
+</html>
